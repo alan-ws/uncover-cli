23fe6d811e1eed6a4078cf199b225341094dc513
alan-william5 <alan.william5@outlook.com>
2023-07-01 22:17:17 +0100

    make it happen

--- src/commnds/link.ts
+++ src/commnds/link.ts
@@ -1,41 +1,66 @@
-import { Separator, select } from "@inquirer/prompts";
-import { Command, Option, OptionValues } from "commander";
-import { exit, prependOnceListener } from "node:process";
-import { streamableExec } from "../utils";
-import path, { delimiter } from "node:path";
+import { Command, Option } from "commander";
+import { exit } from "node:process";
+import path from "node:path";
 import {
   appendFileSync,
   existsSync,
   lstatSync,
-  readFile,
   readFileSync,
-  readdirSync,
   writeFileSync,
 } from "node:fs";
-import XDGAppPaths from "xdg-app-paths";
-import { homedir, platform } from "node:os";
 import { access, constants } from "node:fs/promises";
-
-interface M extends Object, OptionValues { }
-
-function defaultDirectory(value: string) {
-  if (value === ".") return process.cwd();
-  return value;
-}
 
 async function fileExists<T>(dir: string, fileName: T): Promise<Map<T, boolean>> {
   return new Map().set(fileName, existsSync(path.join(dir, (fileName as string))))
 }
 
-async function handle(value: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>, args: string) {
-  if (value.has("uncover.json")) {
-    console.log("creating uncover.json")
-    writeFileSync(
-      path.join(args, "uncover.json"),
-      JSON.stringify({ uncover: "enabled" }),
-      { encoding: "utf-8" }
+async function handleUncover(args: string) {
+  console.log("creating uncover.json")
+  writeFileSync(
+    path.join(args, "uncover.json"),
+    JSON.stringify({
+      uncover: "enabled",
+      projectId: "",
+      teamId: "",
+      gitUserName: ""
+    }),
+    { encoding: "utf-8" }
+  );
+}
+
+async function handleGitIgnore(value: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>, args: string) {
+  if (!value.get('.gitignore')) {
+    console.log("Create .gitignore");
+    writeFileSync(path.join(args, ".gitignore"), "#uncover\nuncover.json", {
+      encoding: "utf-8",
+    });
+  } else {
+    console.log("Add uncover.json to .gitignore");
+    appendFileSync(
+      path.join(args, ".gitignore"),
+      "\n#uncover\nuncover.json"
     );
   }
+}
+
+async function handleVercelIgnore(value: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>, args: string) {
+  if (!value.get('.vercelignore')) {
+    console.log("Create .vercelignore");
+    writeFileSync(path.join(args, ".vercelignore"), "#uncover\nuncover.json", {
+      encoding: "utf-8",
+    });
+  } else {
+    console.log("Add uncover.json to .vercelignore");
+    appendFileSync(
+      path.join(args, ".vercelignore"),
+      "\n#uncover\nuncover.json"
+    );
+  }
+}
+
+function getPID() {
+  const file = JSON.parse(readFileSync(path.join(process.cwd(), '.vercel', 'project.json'), { encoding: 'utf-8' }))
+  return file.projectId
 }
 
 export function link() {
@@ -44,11 +69,28 @@ export function link() {
     "link uncover to vercel project.\nlinks to default hobby unless project ID provided or found in root directory"
   )
 
-  const projectIdOption = new Option("-p --project-id <id>", "vercel project id")
+  const projectIdOption = new Option("-p --project-id [id]", "vercel project id")
+  const patchOption = new Option("--patch", "patch .vercelignore and .gitignore with uncover.json")
   const directoryOption = new Option("-d --directory [directory]", "absolute path to vercel project")
+  const teamIdOption = new Option("-t --team-id [id]", "vercel team id")
 
+  projectIdOption.default("takes value from .vercel/project.json if present")
+  projectIdOption.preset(getPID())
   projectIdOption.argParser(async (op) => {
     console.log('pro', op)
+  })
+
+  patchOption.argParser(async () => {
+    console.log('Patching .gitignore and .vercelignore')
+    try {
+      await Promise.all([
+        handleGitIgnore(new Map().set('.gitignore', true), process.cwd()),
+        handleVercelIgnore(new Map().set('.vercelignore', true), process.cwd())
+      ])
+    } catch (err) {
+      console.error(err)
+      exit(1)
+    }
   })
 
   directoryOption.default(process.cwd(), "takes current root directory")
@@ -80,74 +122,26 @@ export function link() {
     }
 
     const files: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>[] = await Promise.all([
-      fileExists<"uncover.json">(args, "uncover.json"),
       fileExists<".gitignore">(args, ".gitignore"),
       fileExists<".vercelignore">(args, ".vercelignore"),
     ]);
 
-    await Promise.all(files.flatMap((value: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>) => handle(value, args)))
-
-    // files.find((value: Map<'uncover.json' | '.gitignore' | '.vercelignore', boolean>) => value.has())
-    // for (let name of ['uncover.json', '.gitignore', '.vercelignore']) {
-    //   for (let file of files) {
-    //     file.has
-    //   }
-    // }
+    try {
+      await Promise.all([
+        handleUncover(args),
+        handleGitIgnore(files[0], args),
+        handleVercelIgnore(files[1], args)
+      ])
+    } catch (err) {
+      console.error(err)
+      exit(1)
+    }
   })
 
+  program.addOption(teamIdOption)
   program.addOption(projectIdOption)
   program.addOption(directoryOption)
+  program.addOption(patchOption)
 
-  // .action(async (op: M) => {
-  //   if (Object.entries(op).length < 1) {
-  //     program.outputHelp();
-  //     // import { Separator, select } from "@inquirer/prompts";
-  //     // maybe ask if the user would like to link the project and output help on no
-  //   }
-
-  //   if (op.hasOwnProperty("projectId")) {
-  //     console.log(op.projectId);
-  //   }
-
-  // if (op.hasOwnProperty("directory")) {
-  //   let dir = op.directory;
-  //   if (dir === true) dir = process.cwd();
-
-  //   if (files[0] === false) {
-  //     console.log("linking directory\n");
-  //     console.log("Create uncover.json");
-
-
-  //     if (files[1] === false) {
-  //       console.log("Create .gitignore");
-  //       writeFileSync(path.join(dir, ".gitignore"), "uncover.json", {
-  //         encoding: "utf-8",
-  //       });
-  //     } else {
-  //       console.log("Add uncover.json to .gitignore");
-  //       appendFileSync(
-  //         path.join(dir, ".gitignore"),
-  //         "\n#uncover\nuncover.json"
-  //       );
-  //     }
-  //     if (files[2] === false) {
-  //       console.log("Create .vercelignore");
-  //       writeFileSync(path.join(dir, ".vercelignore"), "uncover.json", {
-  //         encoding: "utf-8",
-  //       });
-  //     } else {
-  //       console.log("Add uncover.json to .vercelignore");
-  //       appendFileSync(
-  //         path.join(dir, ".gitignore"),
-  //         "\n#uncover\nuncover.json"
-  //       );
-  //     }
-  //     console.log("\n");
-  //   } else
-  //     console.log(
-  //       "Project is already linked. uncover.json is present at the root of directory\n"
-  //     );
-  // }
-  // });
   return program;
 }
