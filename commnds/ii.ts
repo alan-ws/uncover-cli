f6cd54caf72ea41b2247d55786102448af065a3d
Alan Williams <alan.williams@vercel.com>
2023-07-06 19:06:39 +0100

    changes in my life

---
+++ commnds/ii.ts
@@ -0,0 +1,87 @@
+// .option("-i --install", "Install the core, base components")
+// .option(
+//   "-ii --interactive-install",
+//   "Interactive install - requires complete new install"
+// )
+// .option("-r --run [file] [url]", "Run test on local machine")
+// .action(async () => {
+
+//   let answer: string | undefined;
+//   if (opts.install) {
+//     if (process.platform === "win32") {
+//       answer = await select({
+//         message: "Select your Windows package manner",
+//         choices: [
+//           new Separator("== Package Managers [Enter to select] =="),
+//           { value: "choco" },
+//           { value: "scoop" },
+//           { value: "winget" },
+//           { value: "other" },
+//         ],
+//       });
+//     }
+
+//     if (process.platform === "darwin") {
+//       answer = await select({
+//         message: "Select your Windows package manner",
+//         choices: [
+//           new Separator("== Package Managers [Enter to select] =="),
+//           { value: "brew" },
+//           { value: "other" },
+//         ],
+//       });
+//     }
+
+//     console.log(answer);
+//     if (answer === "other") {
+//       console.log(
+//         "Use the interactive install [uncover -ii] to build from binary"
+//       );
+//       exit;
+//     } else {
+//       const child = exec(`${answer} install k6`, (e, sto, ste) => {
+//         // console.log(sto)
+//       });
+//       child.stdout?.on("data", console.log);
+//     }
+//   }
+//   // if (opts.interactiveInstall) {
+//   //   let filePath = "go".includes(path.sep) ? path.resolve("go") : undefined;
+//   //   if (filePath) await execute(filePath);
+//   //   let dirs = process.env.PATH?.split(path.delimiter);
+//   //   let bins = await Promise.all(dirs!.map(async dir => {
+//   //     try {
+//   //       return await execute(path.join(dir, "go"))
+//   //     } catch (er) {
+//   //       // console.error(er)
+//   //     }
+//   //   }));
+//   //   const goPath = bins.find(bin => !!bin)
+//   // }
+// })
+// .parse(process.argv);
+
+// if (process.argv.slice(2).length) {
+//   if (opts.run) {
+//     program.action(async () => {
+//       if (typeof opts.run === 'string') {
+//         if (opts.run.startsWith('https') || opts.run.includes('.com')) {
+//           const dir = await mkdtemp(join(tmpdir(), 'uncover-'))
+//           await writeFile(`${join(dir, "/script.js")}`, basic(opts.run))
+//           const { stderr, stdout } = await execified(
+//             `k6 run ${join(dir, "/script.js")}`
+//           );
+//           console.log(stdout)
+//           await rm(join(dir, "/script.js"))
+//           await rmdir(dir)
+//           return;
+//         }
+//         const { stderr, stdout } = await execified(
+//           `k6 run ${join(__dirname, opts.run)}`
+//         );
+
+//         console.log(stdout)
+//       }
+//     })
+//   }
+// }
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@
 
 import { Command } from "commander";
 import figlet from "figlet";
-import { exec } from "node:child_process";
-import { promisify } from "node:util";
-import { install } from "./commnds/install";
-import { link } from "./commnds/link";
+import {
+  appendFileSync,
+  existsSync,
+  readFileSync,
+  writeFileSync,
+} from "node:fs";
+import { access, constants, lstat } from "node:fs/promises";
+import path from "node:path";
+import { exit, pid } from "node:process";
 
 let program = new Command();
+
 console.log(figlet.textSync("uncover"));
 
 program
@@ -15,105 +21,156 @@ program
   .version("1.0.0")
   .description("An example CLI for managing a directory")
   .usage("<command> [options] [...]");
-
 program.option("-v, --verbose", "enable verbose mode");
-// .option('-d, --debug', 'enable debug mode');
 
-const installCommand = install();
-const linkCommand = link();
-
-program.addCommand(installCommand);
-program.addCommand(linkCommand);
-program.parse();
-// .option("-i --install", "Install the core, base components")
-// .option(
-//   "-ii --interactive-install",
-//   "Interactive install - requires complete new install"
-// )
-// .option("-r --run [file] [url]", "Run test on local machine")
-// .action(async () => {
-
-//   let answer: string | undefined;
-//   if (opts.install) {
-//     if (process.platform === "win32") {
-//       answer = await select({
-//         message: "Select your Windows package manner",
-//         choices: [
-//           new Separator("== Package Managers [Enter to select] =="),
-//           { value: "choco" },
-//           { value: "scoop" },
-//           { value: "winget" },
-//           { value: "other" },
-//         ],
-//       });
-//     }
-
-//     if (process.platform === "darwin") {
-//       answer = await select({
-//         message: "Select your Windows package manner",
-//         choices: [
-//           new Separator("== Package Managers [Enter to select] =="),
-//           { value: "brew" },
-//           { value: "other" },
-//         ],
-//       });
-//     }
-
-//     console.log(answer);
-//     if (answer === "other") {
-//       console.log(
-//         "Use the interactive install [uncover -ii] to build from binary"
-//       );
-//       exit;
-//     } else {
-//       const child = exec(`${answer} install k6`, (e, sto, ste) => {
-//         // console.log(sto)
-//       });
-//       child.stdout?.on("data", console.log);
-//     }
-//   }
-//   // if (opts.interactiveInstall) {
-//   //   let filePath = "go".includes(path.sep) ? path.resolve("go") : undefined;
-//   //   if (filePath) await execute(filePath);
-//   //   let dirs = process.env.PATH?.split(path.delimiter);
-//   //   let bins = await Promise.all(dirs!.map(async dir => {
-//   //     try {
-//   //       return await execute(path.join(dir, "go"))
-//   //     } catch (er) {
-//   //       // console.error(er)
-//   //     }
-//   //   }));
-//   //   const goPath = bins.find(bin => !!bin)
-//   // }
-// })
-// .parse(process.argv);
-
-// if (process.argv.slice(2).length) {
-//   if (opts.run) {
-//     program.action(async () => {
-//       if (typeof opts.run === 'string') {
-//         if (opts.run.startsWith('https') || opts.run.includes('.com')) {
-//           const dir = await mkdtemp(join(tmpdir(), 'uncover-'))
-//           await writeFile(`${join(dir, "/script.js")}`, basic(opts.run))
-//           const { stderr, stdout } = await execified(
-//             `k6 run ${join(dir, "/script.js")}`
-//           );
-//           console.log(stdout)
-//           await rm(join(dir, "/script.js"))
-//           await rmdir(dir)
-//           return;
-//         }
-//         const { stderr, stdout } = await execified(
-//           `k6 run ${join(__dirname, opts.run)}`
-//         );
-
-//         console.log(stdout)
-//       }
-//     })
-//   }
-// }
-
-if (!process.argv.slice(2).length) {
-  program.outputHelp();
-  console.log("\n");
+function canAccessR_OK(args: string) {
+  try {
+    access(args, constants.R_OK);
+    console.log(`uncover can access: ${args}`);
+  } catch (err) {
+    console.log(`uncover does not have read permissions for: ${args}`);
+    exit(1);
+  }
 }
+
+async function LinkCommand() {
+  const program = new Command("link");
+  program.name("link");
+  program.description("link to your vercel project");
+  program.option(
+    "-p, --project-id [id]",
+    "provide project id",
+    ".vercel/project.json"
+  );
+  program.option(
+    "-t, --team-id [id]",
+    "provide team id",
+    ".vercel/project.json"
+  );
+  program.option(
+    "-g, --git-username [name]",
+    "provide git username",
+    "git config --list"
+  );
+  program.option(
+    "-d, --directory [dir]",
+    "point uncover to directory to link",
+    "."
+  );
+  program.action(async (args) => {
+    let { directory, projectId, teamId, gitUsername } = args;
+    console.log(projectId);
+
+    enum Filenames {
+      uncover = "uncover.json",
+      git = ".gitignore",
+      vercel = ".vercelignore",
+    }
+
+    if (
+      directory === true ||
+      projectId === true ||
+      teamId === true ||
+      gitUsername === true
+    ) {
+      console.log(
+        "link uses default values, if a flag is used, please provide a value"
+      );
+      exit(1);
+    }
+
+    directory = process.cwd();
+
+    if (existsSync(path.join(directory, Filenames.uncover))) {
+      console.log("project is already linked");
+      exit(0);
+    }
+
+    try {
+      (await lstat(directory)).isDirectory();
+    } catch (err) {
+      console.debug("directory check: ", err);
+    }
+
+    canAccessR_OK(directory);
+    writeFileSync(
+      path.join(directory, Filenames.uncover),
+      JSON.stringify({
+        uncover: "enabled",
+        projectId:
+          projectId.match(".vercel/project.json") !== null
+            ? ((): string => {
+                canAccessR_OK(path.join(directory, projectId));
+                const file = JSON.parse(
+                  readFileSync(path.join(directory, projectId), {
+                    encoding: "utf-8",
+                  })
+                );
+                return file.projectId;
+              })()
+            : projectId,
+        teamId: teamId.match(".vercel/project.json") !== null
+          ? ((): string => {
+              canAccessR_OK(path.join(directory, teamId));
+              const file = JSON.parse(
+                readFileSync(path.join(directory, teamId), {
+                  encoding: "utf-8",
+                })
+              );
+              return file.orgId;
+            })()
+          : teamId,
+        // gitUserName: await (async (): string => {
+        //   canAccessR_OK(path.join(directory, ".git"));
+        //   const { stderr, stdout } = await execified("git config --list");
+        //   if (stderr) exit(1);
+        //   console.log(stdout.match(/user\.name=(.*)\n/)![1]);
+        //   return stdout.match(/user\.name=(.*)\n/) === null
+        //     ? null
+        //     : stdout.match(/user\.name=(.*)\n/)![1];
+        // })(),
+      }),
+      { encoding: "utf-8" }
+    );
+
+    if (existsSync(path.join(directory, Filenames.vercel))) {
+      console.log("adding uncover.json to vercelignore");
+      appendFileSync(
+        path.join(directory, Filenames.vercel),
+        "\n#uncover\nuncover.json"
+      );
+    } else {
+      console.log("adding vercelignore and appending uncover.json");
+      writeFileSync(
+        path.join(directory, Filenames.vercel),
+        "#uncover\nuncover.json",
+        {
+          encoding: "utf-8",
+        }
+      );
+    }
+
+    if ((await lstat(path.join(directory, ".git"))).isDirectory()) {
+      if (existsSync(path.join(directory, Filenames.git))) {
+        console.log("adding uncover.json to gitignore");
+        appendFileSync(
+          path.join(directory, Filenames.vercel),
+          "\n#uncover\nuncover.json"
+        );
+      } else {
+        console.log("adding gitignore and appending uncover.json");
+        writeFileSync(
+          path.join(directory, Filenames.vercel),
+          "#uncover\nuncover.json",
+          {
+            encoding: "utf-8",
+          }
+        );
+      }
+    }
+  });
+  return program;
+}
+
+program.addCommand(await LinkCommand());
+await program.parseAsync(process.argv);
